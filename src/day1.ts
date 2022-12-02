708da4f59767874bda0eac90dc0e50e512be7b4a
Jackson Mills <jackson.mills@steelseries.com>
2022-12-02 09:32:49 -0600

    Initial commit

---
+++ src/day1.ts
@@ -0,0 +1,50 @@
+var fs = require("fs");
+
+const readFileAsync = fs.readFileSync("input.txt", "utf8");
+const allLines = readFileAsync.split(/\r\n|\n/);
+
+let mostCalories = 0;
+const elves: string[][] = [[]];
+let elfCount = 0;
+allLines.forEach((line: string) => {
+  if (line !== "") {
+    elves[elfCount].push(line);
+  } else {
+    elves.push([]);
+    elfCount += 1;
+  }
+});
+
+elves.forEach((elf) => {
+  let total = 0;
+  elf.forEach((foodItem) => {
+    const itemAsNum = parseInt(foodItem);
+    total += itemAsNum;
+  });
+  if (total > mostCalories) {
+    mostCalories = total;
+  }
+});
+
+console.log(`the elf with the most has ${mostCalories} calories.`);
+
+const calculateTopThree = (totals: number[]) => {
+  let newTotal = 0;
+  for (let i = 0; i < 3; i++) {
+    newTotal += totals[i];
+  }
+  return newTotal;
+};
+
+const totals: number[] = [];
+elves.forEach((elf) => {
+  let total = 0;
+  elf.forEach((foodItem) => {
+    const itemAsNum = parseInt(foodItem);
+    total += itemAsNum;
+  });
+  totals.push(total);
+});
+const sortedTotals = totals.sort((a, b) => b - a);
+const finalAnswer = calculateTopThree(sortedTotals);
+console.log(`the final answer is ${finalAnswer}`);
